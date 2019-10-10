62f7e666c25065c81e2f1b4d8031de79c83b6bab
MARYRIVERO <marilyndlvalle892286@gmail.com>
2019-10-10 11:45:32 -0500

    Funcionalidades lista

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,75 @@
 window.cipher = {
-  // Acá escribe tu código.
-};
+  encode(myString, param) {
+    let encriptado = "";
+    param = parseInt(param) 
+
+  for (let index = 0; index < myString.length; index++) { // recorrer el string 
+     
+      let numberOfTheLetter = myString.charCodeAt(index) //numero de la letra en el codigo ASCII
+
+      if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90) { 
+      const formule =  (numberOfTheLetter - 65 + param) % 26 + 65; //almacena fórmula de cifrado que se utiliza
+      let newLetter= String.fromCharCode(formule); // almacena valor de la nueva letra cifrada
+      encriptado += newLetter // formar el string cifrado
+    
+     }else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122) { // valor UNICODE de letras minúsculas en ASCII
+       formule = ( numberOfTheLetter - 97 + param) % 26 + 97; // fórmula de cifrado Cesar
+       let newLetter = String.fromCharCode(formule); // valor de letra cifrada
+       encriptado += newLetter; // formar el string cifrado
+
+     }else if(numberOfTheLetter === 32){ // verificar si es un espacio vacio
+       encriptado += ' ';  // añadir espacio en string cifrado
+
+     }else if((numberOfTheLetter >= 48 && numberOfTheLetter <=57)){ // valor UNICODE de números en ASCII
+      formule = ( numberOfTheLetter - 48 + param) % 10 + 48; // fórmula de cifrado Cesar
+      let newLetter = String.fromCharCode(formule); // valor de letra cifrada
+      encriptado += newLetter; // formar el string cifrado
+       
+      }else
+        break;
+    }
+    return encriptado;   // Retorna el valor de la cadena cifrada
+  },
+
+ decode(myString, param) {
+  let desencriptado = "";
+  param = parseInt(param) 
+
+   for (let index = 0; index < myString.length; index++) {   //recorrer el string del usuario
+      let numberOfTheLetter = myString.charCodeAt(index);  //valor UNICODE de la letra en el código ASCII
+      console.log(numberOfTheLetter)
+
+      if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90){  //saber si la letra está en mayúscula
+       
+
+        let formule = (numberOfTheLetter + 65 - param) % 26 + 65 // formula para descifrar
+        let theNewLetter = String.fromCharCode(formule); // obtener el valor de la letra descifrada
+        desencriptado += theNewLetter; // formar la cadena descifrada
+
+      }else if (numberOfTheLetter >= 97 && numberOfTheLetter <=122){ // saber si la letra está en minúscula
+        
+
+        formule = ( numberOfTheLetter + 85 - param) % 26 + 97; // formula para descifrar
+        theNewLetter = String.fromCharCode(formule); // obtener el valor de la letra descifrada
+        desencriptado += theNewLetter; // formar la cadena descifrada
+        
+ 
+      }else if(numberOfTheLetter === 32){ // verificar si es un espacio vacio
+        
+        desencriptado += ' '; // añadir un espacio en string decifrado
+
+      } else if((numberOfTheLetter >= 48 && numberOfTheLetter <=57)){ // valor UNICODE de números en ASCII
+       
+          formule = ( numberOfTheLetter + 52 - param) % 10 + 48; // fórmula de cifrado Cesar
+          newLetter = String.fromCharCode(formule); // valor de letra cifrada
+          desencriptado += newLetter; // formar el string cifrado
+      } else {
+       
+          break;
+        }
+      }
+      return desencriptado;   // Retorna el valor de la cadena cifrada
+    }
+   };
+
+
---
+++ src/index.js
@@ -0,0 +1,92 @@
+
+const btn = document.getElementById('btn');
+const secondView = document.getElementById("secondView");
+const firstView = document.getElementById("firstView");
+let parameter = '';
+
+
+//Dando funcionalidad a mi primer botón
+
+btn.addEventListener('click', () => {
+  parameter = document.getElementById("parameter").value;
+  secondView.classList.remove("ocultar");  
+  firstView.classList.add("ocultar");
+ 
+});
+
+const twoBtn = document.getElementById("twoBtn");
+const thirdView = document.getElementById("thirdView");
+
+//Dando funcionalidad a mi segundo botón y resultados en mi tercera vista
+
+twoBtn.addEventListener('click', () => {
+  const fullName = document.getElementById("fullName").value;
+  const numberIdentity= document.getElementById("numberIdentity").value;
+  const address= document.getElementById("address").value;
+  const phone= document.getElementById("phone").value;
+
+  const fullNameCripter= window.cipher.encode(fullName,parameter);
+  document.getElementById("fullNameRes").value = fullNameCripter;
+
+  const numberIdentityCripter= window.cipher.encode(numberIdentity,parameter);
+  document.getElementById("numberIdentityRes").value = numberIdentityCripter;
+
+  const addressCripter= window.cipher.encode(address,parameter);
+  document.getElementById("addressRes").value = addressCripter;
+
+  const phoneCripter= window.cipher.encode(phone,parameter);
+  document.getElementById("phoneRes").value = phoneCripter;
+
+
+  //Mostrando y ocultando pantallas
+
+  thirdView.classList.remove("ocultar");
+  secondView.classList.add("ocultar");
+  });
+
+const threeBtn = document.getElementById("threeBtn");
+const quarterView = document.getElementById("quarterView");
+
+//Dando funcionalidad a mi tercer botón
+threeBtn.addEventListener('click', () => {
+  const fullNameRes= document.getElementById("fullNameRes").value;
+  const numberIdentityRes= document.getElementById("numberIdentityRes").value;
+  const addressRes= document.getElementById("addressRes").value;
+  const phoneRes= document.getElementById("phoneRes").value;
+
+
+  const fullNameDecripter= window.cipher.decode(fullNameRes,parameter);
+  document.getElementById("fullNameDecode").value = fullNameDecripter;
+
+  const numberIdentityDecripter= window.cipher.decode(numberIdentityRes,parameter);
+  document.getElementById("numberIdentityDecode").value = numberIdentityDecripter;
+
+  const addressDecripter= window.cipher.decode(addressRes,parameter);
+  document.getElementById("addressDecode").value = addressDecripter;
+
+  const phoneDecripter= window.cipher.decode(phoneRes,parameter);
+  document.getElementById("phoneDecode").value = phoneDecripter;
+
+//Mostrando y ocultando pantallas
+  quarterView.classList.remove("ocultar");
+  thirdView.classList.add("ocultar");
+});
+
+//Dando funcionalidad a mi cuarto botón
+quarterBtn.addEventListener('click', () => {
+
+  firstView.classList.remove("ocultar");
+  quarterView.classList.add("ocultar");
+  //Refrescando mi primer botón //
+  const retornar=document.querySelector('input[id= "parameter"]');
+  retornar.value= "";
+  //Refrescando mi segunda vista //
+  const retornarDatos=document.querySelector('input[id="fullName"]');
+  retornarDatos.value= "";
+  const retornarDocument=document.querySelector('input[id="numberIdentity"]');
+  retornarDocument.value= "";
+  const retornarAdress=document.querySelector('input[id="address" ]');
+  retornarAdress.value= "";
+  const retornarPhone=document.querySelector('input[id= "phone"]');
+  retornarPhone.value= "";
+});
